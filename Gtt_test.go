6ec00b52dba79f90cce71ae98fb299671faec4a9
Deniz Adrian <deniz@adrianer.de>
2015-08-11 23:22:36 +0200

    test `Put()` Something in a datastore plx

--- Gtt_test.go
+++ Gtt_test.go
@@ -1,7 +1,24 @@
 package Gtt
 
-import "testing"
+import (
+	"testing"
 
-func TestSomething(t *testing.T) {
-	t.Errorf("something failed\n")
+	"appengine/aetest"
+	"appengine/datastore"
+)
+
+type Something struct {
+	Foo int64
+	Bar string
+}
+
+func TestSomethingInMyDatastore(t *testing.T) {
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sth := Something{Foo: 12345, Bar: "baz"}
+	if _, err = datastore.Put(c, datastore.NewIncompleteKey(c, "Something", nil), &sth); err != nil {
+		t.Fatal(err)
+	}
 }
