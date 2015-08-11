f0b6a169ffd004bf4a95ad6a76510ac98d715bcf
Deniz Adrian <deniz@adrianer.de>
2015-08-11 23:10:25 +0200

    mr. bad interpreter punched me

--- .travis_install.sh
+++ .travis_install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 curl -JLO https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.24.zip
 unzip go_appengine_sdk_linux_amd64-1.9.24.zip
