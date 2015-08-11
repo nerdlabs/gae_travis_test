528de4b6ed3e58b07fc7c1f502d737f800e24b50
Deniz Adrian <deniz@adrianer.de>
2015-08-11 23:08:48 +0200

    GAE'ify

---
+++ .travis_install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+curl -JLO https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.24.zip
+unzip go_appengine_sdk_linux_amd64-1.9.24.zip
---
+++ .travis_script.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+goapp test
